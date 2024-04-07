6b349694c6e3b1c2ba7bd44a5e7148d968953056
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2024-04-07 21:46:25 +0100

    feat: changed imgsrc from blob to dataURL for persistency

--- src/components/ui/dialog/MenuDialog.tsx
+++ src/components/ui/dialog/MenuDialog.tsx
@@ -1,7 +1,12 @@
+import { useActiveTool, useImage } from "../../../store/Store";
+import { generateImage } from "../../../utils/drawings";
+import Links from "../Links";
 import DialogContainer from "./DialogContainer";
 import DialogItem from "./DialogItem";
 
 export default function MenuDialog() {
+  const setImage = useImage((state) => state.setImage);
+  const setActiveTool = useActiveTool((state) => state.setActiveTool);
   return (
     <DialogContainer className="w-[150px] gap-4">
       <DialogItem>
@@ -17,18 +22,27 @@ export default function MenuDialog() {
         <p>Export</p>
       </DialogItem>
       <DialogItem>
-        <p>Upload media</p>
+        <label htmlFor="image" className="cursor-pointer">
+          <p>Upload media</p>
+        </label>
+        <input
+          type="file"
+          name="file"
+          id="image"
+          className="absolute inset-0 invisible size-full z-10 "
+          tabIndex={-1}
+          multiple={false}
+          accept=".jpg,.png"
+          onChange={(e) => {
+            setActiveTool("pointer");
+            if (!e.target.files || !e.target.files[0]) return;
+            const file = e.target.files[0];
+            generateImage(file, setImage);
+          }}
+        />
       </DialogItem>
       <hr />
-      <DialogItem>
-        <p>Github</p>
-      </DialogItem>
-      <DialogItem>
-        <p>Twitter</p>
-      </DialogItem>
-      <DialogItem>
-        <p>Email</p>
-      </DialogItem>
+      <Links />
     </DialogContainer>
   );
 }
